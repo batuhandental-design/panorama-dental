fec99e1d4edd00bf0c56a316f650c9fb64ced8a8
Batuhan Aydın <batuhan.dental@gmail.com>
2026-04-10 06:48:57 +0000

    Visual edits

--- src/components/home/TopBar.jsx
+++ src/components/home/TopBar.jsx
@@ -28,7 +28,7 @@ export default function TopBar() {
             </div>
             <div>
               <p className="text-xs text-gray-400">İstanbul / Turkey</p>
-              <p className="text-xs font-medium">Hali</p>
+              <p className="text-xs font-medium">Haliç</p>
             </div>
           </div>
         </div>
